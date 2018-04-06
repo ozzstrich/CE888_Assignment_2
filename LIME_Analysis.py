3b03db0a8e0361a8397887f18b8c07d53baabfa5
ozzstrich <rahmanosama@me.com>
2018-04-06 15:58:45 +0100

    LIME_Analysis.py

--- LIME_Analysis.py
+++ LIME_Analysis.py
@@ -15,22 +15,24 @@ import numpy as np
 
 print "\n Lime Analysis Starting \n"
 
-experiment = Experiment(api_key="FVJK5r3DO7NBCS6Yt2S8WBu7z", project_name="ce888")
+# Push info to comet
+# experiment = Experiment(api_key="FVJK5r3DO7NBCS6Yt2S8WBu7z", project_name="ce888")
 
 # =================== Get top 5 Predictions using Inception ===================
 inet_model = inc_net.InceptionV3()
 
 
-def transform_img_fn(path_list):
+def transform_img_fn(path_list): #  Transforms image into array of numbers
     out = []
     for img_path in path_list:
-        img = image.load_img(img_path, target_size=(299, 299))
+        img = image.load_img(img_path)  #, target_size=(299, 299))
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
         x = inc_net.preprocess_input(x)
         out.append(x)
     return np.vstack(out)
 
+# Image prediction
 # in range(number of images)
 for i in range(25):
     images = transform_img_fn([os.path.join('Data/Breakout/breakout' + str(i) + '.png')])
@@ -43,11 +45,15 @@ for i in range(25):
 
 explainer = lime_image.LimeImageExplainer()
 
-# Hide color is the color for a superpixel turned OFF. Alternatively, if it is NONE, the superpixel will be replaced by the average of its pixels
+# Hide color is the color for a superpixel turned OFF. Alternatively, if
+# it is NONE, the superpixel will be replaced by the average of its pixels
 explanation = explainer.explain_instance(images[0], inet_model.predict, top_labels=5, hide_color=0, num_samples=1000)
 
 
 from skimage.segmentation import mark_boundaries
 
-temp, mask = explanation.get_image_and_mask(295, positive_only=True, num_features=5, hide_rest=True)
+temp, mask = explanation.get_image_and_mask(
+    295, positive_only=True, num_features=5, hide_rest=True)
+fig = (mark_boundaries(temp / 2 + 0.5, mask))
 plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
+fig.savefig("output.png")
