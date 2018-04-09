6aba4ac3d5114a0d82a3a55bef95337e10657ab4
ozzstrich <rahmanosama@me.com>
2018-04-09 17:11:51 +0100

    updated

--- dense_optical_flow.py
+++ dense_optical_flow.py
@@ -1,9 +1,12 @@
 import cv2
 import numpy as np
 
+frames = []
+
 
 # Read in images and cut into 4 slices (mod so it does it for other games)
 def img_cut():
+
     for i in range(3001):
         image_name = 'Data/Breakout/breakout' + str(i) + '.png'
         img = cv2.imread(image_name)
@@ -22,27 +25,34 @@ def img_cut():
             crop_x += crop_height
             crop_width += crop_height
         # print "\n"
+            frames.append(frame)
     return frame
 
 img_cut()
 
 
-prvs = cv2.cvtColor(img_cut(), cv2.COLOR_BGR2GRAY)
-hsv = np.zeros_like(img_cut())
-hsv[...,1] = 255
-# while(1):
-next = cv2.cvtColor(img_cut(),cv2.COLOR_BGR2GRAY)
-flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-hsv[...,0] = ang * 180 / np.pi / 2
-hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-# cv2.imshow('frame2',bgr)
-# k = cv2.waitKey(30) & 0xff
-# if k == 27:
-# print "k == 27"
-# break
-# elif k == ord('s'):
-cv2.imwrite('opticalfb.png',img_cut())
-cv2.imwrite('opticalhsv.png',bgr)
-prvs = next
+def dense_of():
+    print len(frames)
+    for i in range(len(frames)):
+        prvs = cv2.cvtColor(frames[i], cv2.COLOR_BGR2GRAY)
+        hsv = np.zeros_like(frames[i])
+        hsv[...,1] = 255
+        # while(1):
+        next = cv2.cvtColor(frames[i],cv2.COLOR_BGR2GRAY)
+        flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+        hsv[...,0] = ang * 180 / np.pi / 2
+        hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+        bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+        # cv2.imshow('frame2',bgr)
+        # k = cv2.waitKey(30) & 0xff
+        # if k == 27:
+        # print "k == 27"
+        # break
+        # elif k == ord('s'):
+        if i == 12003:
+            cv2.imwrite('opticalfb_' + str(i) + '.png',frames[i])
+            cv2.imwrite('opticalhsv_' + str(i) + '.png',bgr)
+        prvs = next
+
+dense_of()
